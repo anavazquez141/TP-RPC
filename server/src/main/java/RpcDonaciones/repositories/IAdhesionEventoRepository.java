09a598c8ced13219739838c35cf91f6dda785d6d
Evep7 <onblock07@gmail.com>
2025-10-12 18:28:01 -0300

    Add files via upload

---
+++ server/src/main/java/RpcDonaciones/repositories/IAdhesionEventoRepository.java
@@ -0,0 +1,13 @@
+package RpcDonaciones.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import RpcDonaciones.entities.AdhesionEvento;
+import java.util.List;
+
+@Repository
+public interface IAdhesionEventoRepository extends JpaRepository<AdhesionEvento, Long> {
+    List<AdhesionEvento> findByIdEvento(String idEvento);
+    List<AdhesionEvento> findByIdOrganizacion(String idOrganizacion);
+    List<AdhesionEvento> findByIdVoluntario(String idVoluntario);
+}
